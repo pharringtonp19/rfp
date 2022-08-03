237c0855d71bd62a0c376981f5bc19e28cfb0bb0
Patrick Power <pharringtonp19@gmail.com>
2022-08-03 16:02:28 -0400

    more work

--- rfp/featuremap.py
+++ rfp/featuremap.py
@@ -7,54 +7,54 @@ from dataclasses import dataclass
 from rfp.base import ODE_Solver, Params, Array, Kleisi
 from rfp.nn import MLP # Is this necessary?
 
-
-
 @dataclass 
 class neuralODE:
+    """Neural ODE"""
     mlp: MLP = MLP([32, 1])
     solver: ODE_Solver = Heun()
-    t1: float = 1.0 
+    t1: float = 1.0
 
-#     def vector_field(self, t, y, args):
-#         """mlp parameterized vector field"""
-#         state = jnp.hstack([y, jnp.array(t)])
-#         return self.mlp.fwd_pass(args, state)
-    
-#     def aug_vector_field(self, t, state, args):
-#         """augmented mlp parameterized vector field"""
-#         y, _ = state[:-1], state[-1]
-#         a = self.vector_field(t, y, args)
-#         b = jnp.reshape(jnp.linalg.norm(jax.jacobian(self.vector_field)(t, y, args)), (1,))
-#         c = jnp.concatenate((a, b))
-#         return c
+    def vector_field(self, t, y, args):
+        """mlp parameterized vector field"""
+        state = jnp.hstack([y, jnp.array(t)])
+        return self.mlp.fwd_pass(args, state)
 
-#     def solve_ivp(self, params, input):
-#         """fwd pass of ode"""
-#             return diffeqsolve(
-#                 ODETerm(self.aug_vector_field),
-#                 self.solver,
-#                 t0=0,
-#                 t1=self.t1,
-#                 y0=input,
-#                 dt0=None,
-#                 adjoint=BacksolveAdjoint(),
-#                 stepsize_controller=PIDController(rtol=1e-3, atol=1e-3),
-#                 args=params,
-#             ).ys
+    def aug_vector_field(self, t, state, args):
+        """ Augmented mlp parameterized vector field"""
+        y, _ = state[:-1], state[-1]
+        a = self.vector_field(t, y, args)
+        b = jnp.reshape(jnp.linalg.norm(jax.jacobian(self.vector_field)(t, y, args)), (1,))
+        c = jnp.concatenate((a, b))
+        return c
 
-#     def __call__(self, params: Params, X: Array) -> Kleisi[Array]:
-#         sols = jax.vmap(self.solve_ivp, in_axes=(None, 0))(
-#             params, jnp.hstack((X, jnp.zeros((X.shape[0], 1))))
-#         ).squeeze()
-#         phiX = sols[:, :-1]
-#         regs = sols[:, -1]
-#         return phiX, jnp.mean(regs)
+    def solve_ivp(self, params, input):
+        """fwd pass of ode"""
+        return diffeqsolve(
+            ODETerm(self.aug_vector_field),
+            self.solver,
+            t0=0,
+            t1=self.t1,
+            y0=input,
+            dt0=None,
+            adjoint=BacksolveAdjoint(),
+            stepsize_controller=PIDController(rtol=1e-3, atol=1e-3),
+            args=params,
+        ).ys
 
-# if __name__ == '__main__':
-#     from rfp.nn import MLP 
-#     mlp = MLP([32, 1])
-#     params = mlp.init_fn(jax.random.PRNGKey(0), 2)
-#     x = jnp.linspace(-3,3,10).reshape(-1,1)
-#     feature_map = neuralODE()
-#     yhat, regs = feature_map(params, x)
-#     print(type(yhat), yhat.shape, type(regs), regs.shape)
+    def __call__(self, params: Params, X: Array) -> Kleisi[Array]:
+        """Batched FWD Pass"""
+        sols = jax.vmap(self.solve_ivp, in_axes=(None, 0))(
+            params, jnp.hstack((X, jnp.zeros((X.shape[0], 1))))
+        ).squeeze()
+        phiX = sols[:, :-1]
+        regs = sols[:, -1]
+        return phiX, jnp.mean(regs)
+
+if __name__ == '__main__':
+    from rfp.nn import MLP 
+    mlp = MLP([32, 1])
+    params = mlp.init_fn(jax.random.PRNGKey(0), 2)
+    x = jnp.linspace(-3,3,10).reshape(-1,1)
+    feature_map = neuralODE()
+    yhat, regs = feature_map(params, x)
+    print(type(yhat), yhat.shape, type(regs), regs.shape)
--- rfp/losses.py
+++ rfp/losses.py
@@ -1,4 +1,6 @@
+from functools import partial 
 import jax.numpy as jnp 
+from dataclasses import dataclass
 
 class sqr_error:
     """Square Error """
@@ -11,3 +13,22 @@ class sqr_error:
         inputs, targets = data
         prediction = self.mlp.fwd_pass(params, inputs)
         return jnp.mean((prediction-targets)**2)
+
+@dataclass
+class supervised_loss:
+    linear_layer: callable 
+    feature_map: callable 
+
+    # @jax.jit
+    def supervised_loss(self, params, data):
+        """We implement this function as composition of partially evaluated functions"""
+        Y, D, X = data
+
+        # Partial Evaluation
+        partial_feature_map = partial(self.feature_map, X=X)
+        partial_linear_layer = partial(self.linear_layer, Y, D)
+
+        # Composition
+        phiX, vector_field_penalty = partial_feature_map(params)
+        prediction_error, prediction_penalty = partial_linear_layer(phiX)
+        return prediction_error, vector_field_penalty + prediction_penalty
