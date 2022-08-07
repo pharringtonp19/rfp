e03d0cec849b93516b747ee50138a9f2d5aec62a
Patrick Power <pharringtonp19@gmail.com>
2022-08-07 08:57:22 -0400

    started tests

---
+++ src/rfp/add.py
@@ -0,0 +1,6 @@
+def total(xs: list[float]) -> float: 
+    """Total returns the sum of xs"""
+    result: float = 0.0 
+    for x in xs: 
+        result += x 
+    return result 
---
+++ tests/test1.py
@@ -0,0 +1,10 @@
+from rfp.add import total 
+
+def test_total_empty() -> None: 
+    assert total([]) == 0.0 
+
+def test_total_single_item() -> None:
+    assert total([110.0]) == 110.0
+
+def test_total_with_many_items() -> None:
+    assert total([1.0, 2.0, 3.0]) == 6.0 
