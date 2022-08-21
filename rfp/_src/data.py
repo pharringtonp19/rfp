483f98d096ff0b37f3e85c5836b1c5e935c65728
Patrick Power <pharringtonp19@gmail.com>
2022-08-21 15:19:21 -0400

    added labels

--- rfp/_src/data.py
+++ rfp/_src/data.py
@@ -1,9 +1,14 @@
-import jax 
-import jax.numpy as jnp 
+"""All data files should be placed in here"""
+
+
+import jax
+import jax.numpy as jnp
 from rfp._src.types import Data
+
 
 def f1(x):
     return jnp.log(x**2 + 1.0 + jnp.sin(x * 1.5)) + 1.5
+
 
 def sample1(f, key, features: int) -> Data:
     subkey1, subkey2, subkey3 = jax.random.split(key, 3)
@@ -12,17 +17,21 @@ def sample1(f, key, features: int) -> Data:
     outcome = f(treatment) + jax.random.normal(subkey3)
     return outcome, treatment, covariates
 
-def sample2(key, n: int) -> Data: 
-    xs = jnp.linspace(-1.5, 1.5, n).reshape(-1,1)
-    ys = xs**3 
+
+def sample2(key, n: int) -> Data:
+    xs = jnp.linspace(-1.5, 1.5, n).reshape(-1, 1)
+    ys = xs**3
     return (ys, xs)
+
 
 def sample3(key, features: int) -> Data:
     subkey1, subkey2, subkey3, subkey4 = jax.random.split(key, 4)
     treatment = jax.random.normal(subkey1)
     time = jax.random.bernoulli(subkey2).astype(jnp.float32)
     covariates = jax.random.normal(subkey3, shape=(features,))
-    outcome = 1*time + 1*treatment + 2*treatment*time + jax.random.normal(subkey4)
+    outcome = (
+        1 * time + 1 * treatment + 2 * treatment * time + jax.random.normal(subkey4)
+    )
     return outcome, treatment, time, covariates
 
 
