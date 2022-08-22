9741e4596818f755a5b6120b9729f3012407d582
Patrick Power <pharringtonp19@gmail.com>
2022-08-21 20:02:02 -0400

    added batch sampler

--- rfp/_src/train.py
+++ rfp/_src/train.py
@@ -9,27 +9,36 @@ import jax
 from numpy import float32
 import optax
 import chex
-from dataclasses import dataclass 
+from dataclasses import dataclass
 from rfp._src.types import Params, Data
+from rfp._src.utils import batch_sample
 
 
 @dataclass(frozen=True, slots=True)
 class trainer:
-    loss_fn: callable # This is actually not a callable!
+    loss_fn: callable  # This is actually not a callable!
     opt: optax.GradientTransformation
-    epochs: int 
+    epochs: int
+    init_key: int = jax.random.PRNGKey(0)
+    batch_sampler: callable = batch_sample
+    batch_size: int = 32
 
     def train(self, params: Params, data: Data):
         """Params and Data"""
-        def update_fn(carry, t):
+
+        def update_fn(carry, key):
             params, opt_state = carry
-            loss_values, grads = jax.value_and_grad(self.loss_fn, has_aux=self.loss_fn.aux_status)(params, data)
+            sample = self.batch_sampler(self.batch_size, data, key=key)
+            loss_values, grads = jax.value_and_grad(
+                self.loss_fn, has_aux=self.loss_fn.aux_status
+            )(params, sample)
             updates, opt_state = self.opt.update(grads, opt_state, params)
             params = optax.apply_updates(params, updates)
             return (params, opt_state), loss_values
 
         (opt_params, _), loss_values_history = jax.lax.scan(
-            update_fn, (params, self.opt.init(params)), xs=None, length=self.epochs
+            update_fn,
+            (params, self.opt.init(params)),
+            xs=jax.random.split(self.init_key),
         )
         return opt_params, loss_values_history
-
--- rfp/_src/utils.py
+++ rfp/_src/utils.py
@@ -1,4 +1,6 @@
+from operator import ilshift
 import jax
+import jax.numpy as jnp
 from chex import assert_shape
 
 """
@@ -20,5 +22,31 @@ def batch_sample_time(n):
     return decorator
 
 
-def data_loader(params):
-    pass
+def batch_sample(batch_size, data, *, key):
+    """
+    Instead of greating a data loader as a generator as in https://docs.kidger.site/equinox/examples/train_rnn/
+    during the training loop, we scan over keys and draw samples of `batch_size`
+    """
+    if batch_size == data[0].shape[0]:
+        return data
+    else:
+        perm = jax.random.permutation(key, data[0].shape[0])
+        batch_perm = perm[:batch_size]
+        sample = tuple(array[batch_perm] for array in data)
+        return sample
+
+
+if __name__ == "__main__":
+    """This should be made into a test"""
+    from rfp import sample3
+
+    n = 100
+    batch_size = 32
+    features = 10
+    data = batch_sample_time(n)(sample3)(jax.random.PRNGKey(0), features)
+    sample = batch_sample(batch_size, data, key=jax.random.PRNGKey(1))
+    for i in sample:
+        assert_shape(
+            [sample[0], sample[1], sample[2], sample[3]],
+            [(batch_size, 1), (batch_size, 1), (batch_size, 1), (batch_size, features)],
+        )
