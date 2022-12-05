e1a2f931dcff707b16a0d07311bf9a268dd59eb2
Patrick Power <pharringtonp19@gmail.com>
2022-12-05 08:51:55 -0500

    updates

--- tests/test1.py
+++ tests/test1.py
@@ -1,10 +1,9 @@
-from _src.add import total 
+from typing import List
 
-def test_total_empty() -> None: 
-    assert total([]) == 0.0 
 
-def test_total_single_item() -> None:
-    assert total([110.0]) == 110.0
+def total(xs: List[float]) -> float:
+    return -1.0
 
-def test_total_with_many_items() -> None:
-    assert total([1.0, 2.0, 3.0]) == 6.0 
+
+def test_total() -> None:
+    assert total([1, 2, 3]) == 6
