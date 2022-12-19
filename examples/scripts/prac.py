f83db4c5ebc0890e895ba12a63ddff3f70ece97a
Patrick Power <pharringtonp19@gmail.com>
2022-12-19 10:50:20 -0500

    updates

--- examples/scripts/prac.py
+++ examples/scripts/prac.py
@@ -1,10 +1,12 @@
-def f():
-    print("Hello")
+import jax.numpy as jnp 
+from jax import lax 
+from functools import partial 
+import jax 
+import numpy as np 
+from jax.experimental.maps import xmap 
 
-def g():
-    print("world")
-    return 3 
+# z = jnp.arange(20).reshape(4,5).astype(jnp.float32)
+# print(z)
 
-f()
-g() 
-
+x = np.ones((5,4,3))
+print(np.einsum('ijk -> kji', x))
--- rfp/_src/prac.py
+++ rfp/_src/prac.py
@@ -1,8 +1,34 @@
-s = "hello"
-a = [100, 200, 300]
+import os
 
-def printy(arg):
-    print(f"arg = {arg}")
+import functools
+from typing import Optional
 
-class Classy: 
-    pass 
+import numpy as np
+import timeit
+import jax
+import jax.numpy as jnp
+
+from jax.experimental import mesh_utils
+from jax.sharding import PositionalSharding
+
+devices = mesh_utils.create_device_mesh((4,))
+sharding = PositionalSharding(devices)
+x = jax.random.normal(jax.random.PRNGKey(0), (8192, 8192))
+y = jax.device_put(x, sharding.reshape(-1,1))
+z = jnp.sin(y)
+
+jax.debug.visualize_array_sharding(x) # Implicit print statement
+print(" ")
+jax.debug.visualize_array_sharding(y)
+print(" ")
+jax.debug.visualize_array_sharding(z)
+
+# print(timeit.repeat(stmt="jnp.sin(x)",
+#           number=5,
+#           repeat=5, 
+#           globals=globals()))
+
+# print(timeit.repeat(stmt="jnp.sin(y)",
+#           number=5,
+#           repeat=5, 
+#           globals=globals()))
