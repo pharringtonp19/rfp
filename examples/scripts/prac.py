a4c3199dcf7983256e2905e22989003f5e7f5f30
Patrick Power <pharringtonp19@gmail.com>
2022-12-20 12:16:16 -0500

    updates

--- examples/scripts/prac.py
+++ examples/scripts/prac.py
@@ -1,28 +1,82 @@
-<<<<<<< HEAD
-import jax.numpy as jnp 
-from jax import lax 
-from functools import partial 
-import jax 
-import numpy as np 
-from jax.experimental.maps import xmap 
-
-# z = jnp.arange(20).reshape(4,5).astype(jnp.float32)
-# print(z)
-
-x = np.ones((5,4,3))
-print(np.einsum('ijk -> kji', x))
-=======
 import jax
 import jax.numpy as jnp
-import numpy as np
+from jax.experimental import mesh_utils
+from jax.sharding import PositionalSharding
+
+sharding = PositionalSharding(mesh_utils.create_device_mesh((4,)))
+x = jax.random.normal(jax.random.PRNGKey(0), (8192, 8192))
+x = jax.device_put(x, sharding.reshape(2, 2))
+
+@jax.jit
+def f(x):
+  x = x + 1
+  y = jax.lax.with_sharding_constraint(x, sharding.replicate())
+  return y
 
 
-def f(x, y):
-    return 2 * x * y
+jax.debug.visualize_array_sharding(x)
+y = f(x)
+jax.debug.visualize_array_sharding(y)
+print(x.shape)
+print(y.shape)
+
+# def predict(params, inputs):
+#   for W, b in params:
+#     outputs = jnp.dot(inputs, W) + b
+#     inputs = jnp.maximum(outputs, 0)
+#   return outputs
+
+# def loss(params, batch):
+#   inputs, targets = batch
+#   predictions = predict(params, inputs)
+#   return jnp.mean(jnp.sum((predictions - targets)**2, axis=-1))
+
+# loss_jit = jax.jit(loss)
+# gradfun = jax.jit(jax.grad(loss))
 
 
-x, y = 3, 4
-i32_scalar = jax.ShapeDtypeStruct((), jnp.dtype("int32"))
-f_something = jax.jit(f).lower(i32_scalar, i32_scalar).compile()
-print(type(f_something))
->>>>>>> a65b3e54146f0d2c9b87e3531350b005191d9cca
+# def init_layer(key, n_in, n_out):
+#     k1, k2 = jax.random.split(key)
+#     W = jax.random.normal(k1, (n_in, n_out)) / jnp.sqrt(n_in)
+#     b = jax.random.normal(k2, (n_out,))
+#     return W, b
+
+# def init_model(key, layer_sizes, batch_size):
+#     key, *keys = jax.random.split(key, len(layer_sizes))
+#     params = list(map(init_layer, keys, layer_sizes[:-1], layer_sizes[1:]))
+
+#     key, *keys = jax.random.split(key, 3)
+#     inputs = jax.random.normal(keys[0], (batch_size, layer_sizes[0]))
+#     targets = jax.random.normal(keys[1], (batch_size, layer_sizes[-1]))
+
+#     return params, (inputs, targets)
+
+# layer_sizes = [784, 8192, 8192, 8192, 10]
+# batch_size = 8192
+
+# params, batch = init_model(jax.random.PRNGKey(0), layer_sizes, batch_size) # WHERE ARE THESE LOCATED
+# devices = mesh_utils.create_device_mesh((4,))
+# sharding = PositionalSharding(devices).reshape(4, 1)
+# batch = jax.device_put(batch, sharding)
+# #params = jax.device_put(params, sharding.replicate())
+# print(loss_jit(params, batch))
+
+# step_size = 1e-5
+
+# for _ in range(30):
+#   grads = gradfun(params, batch)
+#   params = [(W - step_size * dW, b - step_size * db)
+#             for (W, b), (dW, db) in zip(params, grads)]
+
+# print(loss_jit(params, batch))
+
+
+
+# sharding = PositionalSharding(devices)
+# sharding = sharding.reshape(2, 2)
+# print(sharding)
+# x = jax.random.normal(jax.random.PRNGKey(0), shape=(12,12))
+# jax.debug.visualize_array_sharding(x)
+# y = jax.device_put(x, sharding.replicate(axis=0, keepdims=True))
+# jax.debug.visualize_array_sharding(y)
+
