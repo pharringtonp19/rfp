7e565a305fe1b5bc198684a1510143ce954f0b3d
pharringtonp19 <pharringtonp19@gmail.com>
2023-07-09 15:19:39 -0400

    updates

--- rfp/_src/j.py
+++
@@ -1,6 +0,0 @@
-def double(n):
-    return n * 2
-
-
-num = double(21)
-print(num)
--- rfp/_src/nn.py
+++ rfp/_src/nn.py
@@ -1,26 +1,8 @@
-"""
-
-Examples:
-    >>> import jax 
-    >>> import jax.numpy as jnp 
-    >>> import flax.linen as nn
-    >>> from typing import Sequence
-    >>> from flax.core import unfreeze
-
-    >>> n, d = 100, 10 # (observations, features)
-    >>> mlp = MLP([32, 1], activation=nn.relu) 
-    >>> xs = jax.random.normal(jax.random.PRNGKey(0), (n, d))
-    >>> params = mlp.init_fn(jax.random.PRNGKey(1), d)
-    >>> yhat = mlp.fwd_pass(params, xs)
-"""
 from typing import Callable, Sequence
-
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
 from flax.core import unfreeze
-
-from rfp._src.types import Array
 
 
 class MLP(nn.Module):
@@ -28,7 +10,7 @@ class MLP(nn.Module):
     activation: Callable = nn.relu
 
     @nn.compact
-    def __call__(self, x: Array):
+    def __call__(self, x):
         for feat in self.features[:-1]:
             x = self.activation(nn.Dense(feat)(x))
         x = nn.Dense(self.features[-1])(x)
@@ -38,17 +20,12 @@ class MLP(nn.Module):
         """The Forward Pass"""
         return self.apply({"params": params}, x)
 
-    def init_fn(self, key, features):
+    def init_fn(self, key, features, verbose=False):
         """Initialize Parameters"""
-        params = unfreeze(self.init(key, jnp.ones((1, features))))["params"]
+        params = unfreeze(self.init(key, jnp.ones((features,))))["params"]
+        if verbose:
+            print(jax.tree_util.tree_map(lambda x: x.shape, params)) # Checking output shapes
         return params
 
     def embellished_fwd_pass(self, params, x):
         return self.fwd_pass(params, x), 0.0
-
-
-if __name__ == "__main__":
-    width = 32
-    mlp = MLP([width, width])
-    print(mlp)
-    print(mlp.init_fn(jax.random.PRNGKey(0), 1))
