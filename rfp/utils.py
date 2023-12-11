e2ced6b8728c40c2f9e269442924d491b6b31132
pharringtonp19 <pharringtonp19@gmail.com>
2023-12-10 19:20:08 -0500

    updates

--- rfp/utils.py
+++ rfp/utils.py
@@ -20,7 +20,7 @@ class Model_Params(NamedTuple):
         bias = jax.random.normal(k2, (1, head_dim))    ### THIS NEEDS TO BE CHECKED
         return Model_Params(body, head, bias)
     
-    def shapes(self):
+    def shape(self):
         """Return the shapes of the model parameters."""
         body_shapes = {k: v.shape for k, v in self.body.items()} if self.body else {}
         return {'body': body_shapes, 'head': self.head.shape, 'bias': self.bias.shape}
