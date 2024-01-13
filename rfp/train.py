b2f496f75ff2461bc269e5eb0b4965893fa0a13b
pharringtonp19 <pharringtonp19@gmail.com>
2024-01-13 10:43:05 -0500

    updates to trainer

--- rfp/train.py
+++ rfp/train.py
@@ -12,10 +12,10 @@ from functools import partial
 from typing import Any, Callable, Dict
 
 import jax
+import jax.numpy as jnp 
 import optax
 from jax.experimental import checkify
 from rfp.utils import Model_Params
-
 
 
 @dataclass
@@ -28,14 +28,29 @@ class Trainer:
     def train(self, params: Model_Params, X, Y, mask):
         def update_fn(carry, t):
             params, opt_state = carry
-            loss_values, grads = jax.value_and_grad(
-                self.loss_fn, has_aux=self.loss_fn.aux_status
-            )(params, X, Y, mask)
+            loss_values, grads = jax.value_and_grad(self.loss_fn, has_aux=self.loss_fn.aux_status)(params, X, Y, mask)
             updates, opt_state = self.opt.update(grads, opt_state, params)
             params = optax.apply_updates(params, updates)
             return (params, opt_state), loss_values
-
-        (opt_params, _), loss_values_history = jax.lax.scan(
-            update_fn, (params, self.opt.init(params)), xs=None, length=self.epochs
-        )
+        (opt_params, _), loss_values_history = jax.lax.scan(update_fn, (params, self.opt.init(params)), xs=None, length=self.epochs)
         return opt_params, loss_values_history
+    
+@dataclass
+class Trainer_With_Val:
+    loss_fn: Callable
+    opt: optax.GradientTransformation
+    epochs: int
+
+    # Train Function
+    def train(self, params: Model_Params, X, Y, mask, X_val, Y_val, mask_val):
+        def update_fn(carry, t):
+            params, val_opt_params, opt_val_loss, opt_state = carry
+            loss_values, grads = jax.value_and_grad(self.loss_fn, has_aux=self.loss_fn.aux_status)(params, X, Y, mask)
+            val_losses = jax.vmap(self.loss_fn, in_axes=(None, 0, 0, 0))(params, X_val, Y_val, mask_val)
+            updates, opt_state = self.opt.update(grads, opt_state, params)
+            params = optax.apply_updates(params, updates)
+            val_opt_params = jnp.where(val_losses < opt_val_loss, params, val_opt_params)
+            opt_val_loss = jnp.where(val_losses < opt_val_loss, val_losses, opt_val_loss)
+            return (params, val_opt_params, opt_val_loss, opt_state), (loss_values, val_losses)
+        (final_train_params, val_opt_params, _, _), loss_values_history = jax.lax.scan(update_fn, (params, self.opt.init(params)), xs=None, length=self.epochs)
+        return final_train_params, val_opt_params, loss_values_history
