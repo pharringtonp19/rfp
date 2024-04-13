26b08fbc6bdb9aa3724feb62a23c6d91278ad935
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-13 06:51:31 -0400

    Create setup_env.sh

---
+++ getting-started/setup_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Name of the virtual environment
+ENV_NAME="rfp_env"
+
+# Create virtual environment
+python3 -m venv $ENV_NAME
+
+# Activate the virtual environment
+source $ENV_NAME/bin/activate
+
+# Install any necessary Python packages
+pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install git+https://github.com/pharringtonp19/rfp.git
+pip install matplotlib
+
+echo "Setup complete and the virtual environment $ENV_NAME is ready to use."
