68f5338a040acc92165d71a3b073ea54c72e4d1c
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-04-13 09:03:35 -0400

    Update setup_env.sh

--- getting-started/setup_env.sh
+++ getting-started/setup_env.sh
@@ -12,5 +12,4 @@ source $ENV_NAME/bin/activate
 # Install any necessary Python packages
 pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install optax, flax
-pip install git+https://github.com/pharringtonp19/rfp.git
 pip install matplotlib
